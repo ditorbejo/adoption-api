337134002410bda7c9423e584dd52a6b325994f0
ditorbejo <ditoooriq@gmail.com>
2023-12-19 19:58:52 +0700

    api terbaru update router

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\API\AdoptionController;
 use App\Http\Controllers\API\AnnouncementController;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\GalleryController;
+use App\Http\Controllers\API\ChatsController;
+use App\Http\Controllers\API\Auth\RoleController;
+use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\Auth\RegisterController;
 use App\Http\Controllers\API\PetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,12 +24,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/getAllColor',[PetController::class , 'getAllColor']);
-Route::get('/chat', [\App\Http\Controllers\Api\ChatsController::class, 'index']);
-Route::get('/getAllUser', [\App\Http\Controllers\Api\ChatsController::class, 'getAllUser']);
-Route::post('/messages', [\App\Http\Controllers\Api\ChatsController::class, 'sendMessageUser']);
-Route::post('/messages/{userId}', [\App\Http\Controllers\Api\ChatsController::class, 'sendMessageAdmin']);
-Route::get('/messages', [\App\Http\Controllers\Api\ChatsController::class, 'fetchMessages']);
-Route::get('/messages/{userId}', [\App\Http\Controllers\Api\ChatsController::class, 'fetchMessagesAdmin']);
+Route::get('/chat', [ChatsController::class, 'index']);
+Route::get('/getAllUser', [ChatsController::class, 'getAllUser']);
+Route::post('/messages', [ChatsController::class, 'sendMessageUser']);
+Route::post('/messages/{userId}', [ChatsController::class, 'sendMessageAdmin']);
+Route::get('/messages', [ChatsController::class, 'fetchMessages']);
+Route::get('/messages/{userId}', [ChatsController::class, 'fetchMessagesAdmin']);
 
 Route::get('/test-broadcast-event', function () {
     ChatCattery::dispatch('ini adalah parameter message chat cattery');
@@ -33,9 +37,9 @@ Route::get('/test-broadcast-event', function () {
     echo 'test broadcast event chat cattery';
 });
 
-Route::get('auth/role',\App\Http\Controllers\Api\Auth\RoleController::class);
-Route::post('auth/register', \App\Http\Controllers\Api\Auth\RegisterController::class);
-Route::post('auth/login', \App\Http\Controllers\Api\Auth\LoginController::class);
+Route::get('auth/role',RoleController::class);
+Route::post('auth/register',RegisterController::class);
+Route::post('auth/login', LoginController::class);
 Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
     $request->user()->tokens()->delete();
 
